f49c2e9cc253645d91497992593ab1a49de1bc83
Aerodinn <aerodindesigns@gmail.com>
2026-04-03 14:12:46 +0300

    Update MobilePreview.tsx

--- components/preview/MobilePreview.tsx
+++ components/preview/MobilePreview.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { RobloxGame, UserGame } from "@/lib/roblox";
+import { RobloxGame } from "@/lib/roblox";
+import type { UserGame } from "./PreviewTool";
 
 interface Props {
   userGame: UserGame;
